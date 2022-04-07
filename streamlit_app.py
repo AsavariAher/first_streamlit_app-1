bf63fe86f67249002def8baaf6d3614bf682fb65
MellyMel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2022-04-07 15:24:38 -0700

    Update streamlit_app.py
    dataframe of csv from s3 bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 import pandas
+dataframe = pandas.read_csv(")
+return df.set_index("https://uni-lab-files.s3.us-west-2.amazonaws.com/smew/country_code_to_currency_code.csv")
+
+
