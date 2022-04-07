5fd232607c2a22c7c0629bb7ebad23922e2b82f1
MellyMel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2022-04-07 15:31:26 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 import pandas
-@st.cache
 df = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/smew/country_code_to_currency_code.csv")
 return df.set_index("Country")
 
