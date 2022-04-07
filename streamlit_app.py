dcc8c832aebb5cb697d201a6feddd49b8c070e3a
MellyMel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2022-04-07 15:39:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,18 +17,18 @@ from urllib.error import URLError
 @streamlit.cache
 def get_UN_data():
     AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-    df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
+    dataframe = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
     return df.set_index("Region")
 
 try:
-    df = get_UN_data()
-    countries = st.multiselect(
+    dataframe = get_UN_data()
+    countries = streamlit.multiselect(
         "Choose countries", list(df.index), ["China", "United States of America"]
     )
     if not countries:
-        st.error("Please select at least one country.")
+        streamlit.error("Please select at least one country.")
     else:
-        data = df.loc[countries]
+        data = dataframe.loc[countries]
         data /= 1000000.0
         st.write("### Gross Agricultural Production ($B)", data.sort_index())
 
@@ -47,7 +47,7 @@ try:
         )
         st.altair_chart(chart, use_container_width=True)
 except URLError as e:
-    st.error(
+    streamlit.error(
         """
         **This demo requires internet access.**
 
