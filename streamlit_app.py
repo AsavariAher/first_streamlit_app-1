f23f73d5ebb3925da25be5e69c522a80d1ddbae1
MellyMel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2022-04-07 17:17:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,22 +10,39 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('Build Your Own Fruit Smoothie')
 
 import pandas 
+import altair 
+
+from urllib.error import URLError
+
 @streamlit.cache
 def get_data():
     dataframe = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
     return dataframe.set_index("Fruit")
 
+try:
     dataframe = get_data()
-    smoothiefruitschosen = streamlit.multiselect(
-        "Select Fruits for Your Smoothie", list(dataframe.index), ["Apple", "Banana"]
+    bowlingredients = streamlit.multiselect(
+        "Build Your Own Fruit Bowl", list(dataframe.index), ["Apple", "Banana"]
     )
-        dataset = dataframe.loc[smoothiefruitschosen]
-        streamlit.write("### Fruits in Smoothie", dataset.sort_index())
+    if not bowlingredients:
+        streamlit.error("Please select at least one fruit.")
+    else:
+        dataset = dataframe.loc[bowlingredients]
+        #dataset /= 1000000.0
+        streamlit.write("### Fruits in BYOB", dataset.sort_index())
 
         dataset = dataset.T.reset_index()
         dataset = pandas.melt(dataset, id_vars=["index"]).rename(
-            columns={"index": "Calories", "value": "Fruits in Smoothie"}
+            columns={"index": "Calories", "value": "Fruits in BYOB"}
         )
 
-        )
+except URLError as e:
+    streamlit.error(
+        """
+        **This demo requires internet access.**
+
+        Connection error: %s
+    """
+        % e.reason
+    )
 
