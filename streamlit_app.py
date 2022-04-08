776fa39959ad6e41515598c2cad78e1bde782e26
MellyMel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2022-04-07 19:29:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,31 +12,11 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas 
-from urllib.error import URLError
 
-@streamlit.cache
-def get_data():
-    dataframe = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-    return dataframe.set_index("Fruit")
+dataframe = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+return dataframe.set_index("Fruit")
 
-try:
-    dataframe = get_data()
-    fruits_selected = streamlit.multiselect(
-        "Select some fruits for your smoothie", list(dataframe.index), ["Strawberries", "Banana"]
-    )
-    if not fruits_selected:
-        streamlit.error("It's not really a fruit smoothie, if you don't include a fruit!")
-    else:
-        dataset = dataframe.loc[fruits_selected]
-        streamlit.write("#### Fruits You've Selected:", dataset.sort_index())
 
-except URLError as e:
-    streamlit.error(
-        """
-        **Something went wrong.**
 
-        Connection error: %s
-    """
-        % e.reason
-    )
+
 
