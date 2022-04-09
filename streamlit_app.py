24168f0bbfc191e86612667e6738b79f41a37061
MellyMel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2022-04-08 18:06:57 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,5 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-streamlit.text(fruityvice_response.json_normalize())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response)
 streamlit.text(fruityvice_normalized)
