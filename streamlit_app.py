0f2b35d067bfe99ae3fe26b857caac1750226b26
MellyMel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2022-04-09 08:00:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,10 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
 for row in my_data_rows:
     st.write(f"{row[0]} has a :{row[1]}:")
 
-streamlit.dataframe(my_data_rows)
+
 
