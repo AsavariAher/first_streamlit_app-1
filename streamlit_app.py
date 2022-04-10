25e64b60b466367e2dbbad7b59b498e9607e3f5e
MellyMel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2022-04-09 17:37:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
@@ -26,21 +27,14 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-from urllib.error import URLError
-#Fruityvice data function - repeats each time user hits enter in entry box
-@streamlit.cache
-
-def get_fruityvice_data(fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
-
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
    else:
-        streamlit.dataframe(get_fruityvice_data(fruit_choice))
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
     streamlit.error(
@@ -50,9 +44,6 @@ except URLError as e:
     """
         % e.reason
     )
-
-
-
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
